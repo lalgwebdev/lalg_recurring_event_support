af54e4ceec73d9b2388a614e2a725be2091e5422
Rotekian <61169028+Rotekian@users.noreply.github.com>
2024-06-24 16:33:30 +0100

    Fixed jquery.once() issue.

--- js/lalg_recurring_event_support_create_form.js
+++ js/lalg_recurring_event_support_create_form.js
@@ -5,19 +5,27 @@
    */
   Drupal.behaviors.recurring_events_other_dates = {
     attach: function (context, settings) {
-      $('#edit-weekly-recurring-date-wrapper, #edit-monthly-recurring-date-wrapper, #edit-custom-date-wrapper').find('input.form-date').once().on('change', function (e) {
-        if ($(this).attr('name').includes('[value][date]')) {
-          var start_date = this;
-          var parent = $(this).closest('.form-wrapper');
-          $(parent).find('input.form-date').each(function (index, item) {
-            if (index == 1) {
-              if ($(item).val() == '') {
-                $(item).val($(start_date).val());
+      const wrappers = [
+        'edit-weekly-recurring-date-wrapper',
+        'edit-monthly-recurring-date-wrapper',
+        'edit-custom-date-wrapper'
+      ];
+      wrappers.forEach(wrapper => {
+        const inputElements = document.querySelectorAll(`#${wrapper} input.form-date`);
+        inputElements.forEach(inputElement => {
+          inputElement.addEventListener('change', function(e) {
+            var start_date = this;
+            var parent = $(this).closest('.form-wrapper');
+            $(parent).find('input.form-date').each(function (index, item) {
+              if (index == 1) {
+                if ($(item).val() == '') {
+                  $(item).val($(start_date).val());
+                }
               }
-            }
+            });
           });
-        }
+        });
       });
     }
-  };
+  }
 }(jQuery));
