9d41b0d3abdc2e3b4211baf5576fdbdb27afe10c
Rotekian <61169028+Rotekian@users.noreply.github.com>
2025-09-29 12:47:03 +0100

    autofill admin title and daily fix

--- js/lalg_recurring_event_support_create_form.js
+++ js/lalg_recurring_event_support_create_form.js
@@ -1,16 +1,19 @@
 (function ($) {
   'use strict';
    /**
-   * Set end date for other dates to be the same as the start.
+   * Set end date for other dates to be the same as the start. Also set admin title.
    */
   Drupal.behaviors.recurring_events_other_dates = {
     attach: function (context, settings) {
       const wrappers = [
+        'edit-daily-recurring-date-wrapper',
         'edit-weekly-recurring-date-wrapper',
         'edit-monthly-recurring-date-wrapper',
         'edit-custom-date-wrapper'
       ];
       wrappers.forEach(wrapper => {
+        const adminTitle = document.querySelector(`#${'edit-title-wrapper'} input.form-text`);
+        const seriesTitle = document.querySelector(`#${'edit-field-series-title-wrapper'} input.form-text`);
         const inputElements = document.querySelectorAll(`#${wrapper} input.form-date`);
         inputElements.forEach(inputElement => {
           inputElement.addEventListener('change', function(e) {
@@ -18,9 +21,8 @@
             var parent = $(this).closest('.form-wrapper');
             $(parent).find('input.form-date').each(function (index, item) {
               if (index == 1) {
-                //if ($(item).val() == '') {
-                  $(item).val($(start_date).val());
-                //}
+                $(item).val($(start_date).val());
+                adminTitle.value = ($(start_date).val()+" "+seriesTitle.value);
               }
             });
           });
