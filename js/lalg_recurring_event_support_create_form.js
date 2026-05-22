7a2114257715fa82c310611f42500d7e0683a139
Rotekian <61169028+Rotekian@users.noreply.github.com>
2026-05-22 10:28:22 +0100

    Update lalg_recurring_event_support_create_form.js
    Sync Time in addition to date.

--- js/lalg_recurring_event_support_create_form.js
+++ js/lalg_recurring_event_support_create_form.js
@@ -1,33 +1,39 @@
 (function ($) {
   'use strict';
-   /**
-   * Set end date for other dates to be the same as the start. Also set admin title.
+
+  /**
+   * Set end date and time for other dates/times to be the same as the start. 
+   * Also set admin title.
    */
   Drupal.behaviors.recurring_events_other_dates = {
     attach: function (context, settings) {
       const wrappers = [
-        'edit-daily-recurring-date-wrapper',
-        'edit-weekly-recurring-date-wrapper',
-        'edit-monthly-recurring-date-wrapper',
-        'edit-custom-date-wrapper'
-      ];
-      wrappers.forEach(wrapper => {
-        const adminTitle = document.querySelector(`#${'edit-title-wrapper'} input.form-text`);
+  	    'edit-daily-recurring-date-wrapper',
+  	    'edit-weekly-recurring-date-wrapper',
+  	    'edit-monthly-recurring-date-wrapper',
+  	    'edit-custom-date-wrapper'
+  	  ];
+
+  	  wrappers.forEach(wrapper => {
+  	    const adminTitle = document.querySelector(`#${'edit-title-wrapper'} input.form-text`);
         const seriesTitle = document.querySelector(`#${'edit-field-series-title-wrapper'} input.form-text`);
-        const inputElements = document.querySelectorAll(`#${wrapper} input.form-date`);
-        inputElements.forEach(inputElement => {
-          inputElement.addEventListener('change', function(e) {
-            var start_date = this;
-            var parent = $(this).closest('.form-wrapper');
-            $(parent).find('input.form-date').each(function (index, item) {
-              if (index == 1) {
-                $(item).val($(start_date).val());
-                adminTitle.value = ($(start_date).val()+" "+seriesTitle.value);
-              }
-            });
-          });
-        });
-      });
+  	    const dateInput = document.querySelector(`#${wrapper} input.form-date`);
+  	    const timeSelect = document.querySelector(`#${wrapper} select[data-drupal-selector$="time"]`);
+
+  	    dateInput.addEventListener('change', function(e) {
+  		  var parent = $(this).closest('.fieldset__wrapper');
+  		  var newValue = $(this).val(); 
+		  $(parent).find('input.form-date:eq(1)').val(newValue);
+		  adminTitle.value = (newValue + " " + seriesTitle.value);
+		});
+		if(timeSelect) {
+          timeSelect.addEventListener('change', function(e) {
+		    var parent = $(this).closest('.fieldset__wrapper');
+		    var newValue = $(this).val(); 
+		    $(parent).find('select[data-drupal-selector$="time"]:eq(1)').val(newValue);
+	      });
+        }
+	  });
     }
   }
 }(jQuery));
